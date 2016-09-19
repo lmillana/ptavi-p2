0513f7a8f90244ef30b64cc64d1df448c3745c3a
lmillana <leyreeemcaballero@gmail.com>
2016-09-19 16:26:50 +0200

    Ejercicio 4 Practica 2

---
+++ calcoo.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+class Calculadora():
+
+    def sum(self, op1, op2):
+        return op1 + op2
+
+    def rest(self, op1, op2):
+        return op1 - op2
+
+if __name__ == "__main__":
+
+    calculadora = Calculadora()
+    try:
+        operando1 = int(sys.argv[1])
+        operando2 = int(sys.argv[3])
+    except ValueError:
+        sys.exit("Error: Non numerical parameters.")
+
+    if sys.argv[2] == "suma":
+        result = calculadora.sum(operando1,operando2)
+    elif sys.argv[2] == "resta":
+        result = calculadora.rest(operando1,operando2)
+    else:
+        sys.exit("Gramatic Error")
+
+    print(result)
+        
