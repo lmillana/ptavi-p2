db2462eb7e712fcbd759b0ff8eabada612aad5d2
lmillana <leyreeemcaballero@gmail.com>
2016-09-26 17:11:15 +0200

    Ejercicio 5 Practica 2 Corregido

--- calcoohija.py
+++ calcoohija.py
@@ -2,15 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import sys
-
 import calcoo
+
 
 class CalculadoraHija(calcoo.Calculadora):
 
     def mult(self, op1, op2):
         return op1 * op2
 
-    def div(self, op1 ,op2):
+    def div(self, op1, op2):
         return op1 / op2
 
 if __name__ == "__main__":
@@ -23,17 +23,21 @@ if __name__ == "__main__":
         sys.exit("Error: Non numerical parameters.")
 
     if sys.argv[2] == "suma":
-        result = calculadorahija.sum(operando1,operando2)
+        result = calculadorahija.sum(operando1, operando2)
     elif sys.argv[2] == "resta":
-        result = calculadorahija.rest(operando1,operando2)
+        result = calculadorahija.rest(operando1, operando2)
     elif sys.argv[2] == "multiplica":
-        result = calculadorahija.mult(operando1,operando2)
+        result = calculadorahija.mult(operando1, operando2)
     elif sys.argv[2] == "divide":
         if sys.argv[3] == "0":
-            sys.exit("Error.Divide by zero.")
+            sys.exit("Division by zero is not allowed.")
         else:
-            result = calculadorahija.div(operando1,operando2)
+            result = calculadorahija.div(operando1, operando2)
     else:
         sys.exit("Gramatical Error.Try again.")
 
     print(result)
+
+    """Cuando queremos imprimir llamamos a calculadorahija,
+    que es quien contiene todas las operaciones. Si llamamos solo
+    a calculadora, no funcionaria el programa completo."""
